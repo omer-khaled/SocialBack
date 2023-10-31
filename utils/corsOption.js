935b22f238c84241ccb7439da6a9b48b09b97b2a
omer-khaled <omerkhaledmohamedmohamed123@gmail.com>
2023-10-31 22:35:37 +0200

    first commit last last

--- utils/corsOption.js
+++ utils/corsOption.js
@@ -1,5 +1,5 @@
 const corsOption = {
-    origin:'https://social-front-six.vercel.app',
+    origin:['https://social-front-six.vercel.app','http://localhost:5173'],
     methods:'OPTIONS,GET,PUT,POST,PATHCH,DELETE',
     allowedHeaders:'Content-Type,Authorization',
     credentials:true,
